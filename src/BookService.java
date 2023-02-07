6b327189dddbcac86668661d9b21e494f6649945
Cemil <okcuoglucemil@gmail.com>
2023-02-07 13:35:20 +0300

    Books are created

--- src/BookService.java
+++ src/BookService.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookService implements ProductService{
+    List<Book> bookList = new ArrayList<>();
+    public BookService(){
+        Book book1 = new Book(1,"Hayvan Çiftliği",15.50,10,"George Orwell","Can");
+        Book book2 = new Book(2,"Satranç",8.50,10,"Stefan Zweig","İş Bankası");
+        Book book3 = new Book(3,"Dönüşüm",10.50,10,"Franz Kafka","İş Bankası");
+        Book book4 = new Book(4,"Kuyucaklı Yusuf",13.50,10,"Sabahattin Ali","YKY");
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     public void addProduct() {
 
@@ -11,6 +39,13 @@ public class BookService implements ProductService{
 
     @Override
     public void processMenu() {
+
+
+
+
+
+
+
 
     }
 
