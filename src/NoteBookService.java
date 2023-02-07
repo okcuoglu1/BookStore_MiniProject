48261fb028fc9b55205bf8c724199aa7556e3372
Cemil <okcuoglucemil@gmail.com>
2023-02-07 14:09:31 +0300

    listProduct method for notebook

--- src/NoteBookService.java
+++ src/NoteBookService.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class NoteBookService implements ProductService{
 
+    Scanner scanner = new Scanner(System.in);
     List<Notebook> notebookList = new ArrayList<>();
 
     public NoteBookService() {
@@ -16,6 +18,21 @@ public class NoteBookService implements ProductService{
 
     @Override
     public void addProduct() {
+        System.out.print("Notebook Name: ");
+        String notebookName = scanner.nextLine();
+        System.out.print("Notebook Price: ");
+        scanner.nextLine(); //dummy
+        double notebookPrice = scanner.nextDouble();
+        System.out.print("Stock: ");
+        int stock = scanner.nextInt();
+        System.out.print("Brand Name: ");
+        String brandName = scanner.nextLine();
+        System.out.print("Publisher: ");
+        String sheet = scanner.nextLine();
+
+        Notebook newNoteBook = new Notebook(this.notebookList.get(notebookList.size()-1).getId(),notebookName,notebookPrice,stock,brandName,sheet);
+        this.notebookList.add(newNoteBook);
+        listProduct();
 
     }
 
@@ -36,6 +53,19 @@ public class NoteBookService implements ProductService{
 
     @Override
     public void listProduct() {
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        System.out.printf("%-2s | %-15s | %-15s | %-10s | %-7s | %-5s \n","ID","Notebook Name","Brand Name","Sheet","Price","Stock");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+        for (Notebook noteBook : this.notebookList){
+
+            System.out.printf("%-2s | %-15s | %-15s | %-10s | %-7s | %-5s \n",
+                    noteBook.getId(),noteBook.getName(),noteBook.getBrand(),noteBook.getSheet(),noteBook.getPrice(),noteBook.getStock());
+
+
+
+
+        }
 
     }
 }
